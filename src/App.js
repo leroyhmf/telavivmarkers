4441e371a69afb7aad555461cb3a5e7e21f0c6ce
Leroy Haimoff <leroyblergh@gmail.com>
2018-07-16 18:40:08 +0300

    fix: Set bounds correctly on filtering markers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import { DebounceInput } from 'react-debounce-input';
 import './App.css';
 
 class App extends Component {
@@ -25,29 +26,44 @@ class App extends Component {
         sciences. The place holds science conventions for everyone and teaches high schoolers arriving from schools from all
         around town three main curriculums Physics, Chemistry and Computer Sciences.`}
       ],
-      markerClicked: false
+      markerClicked: false,
+      shownMarkers: false
   }
   this.changeMarkerClicked = this.changeMarkerClicked.bind(this);
+  this.changeShownMarkers = this.changeShownMarkers.bind(this);
+  this.filterMarkers = this.filterMarkers.bind(this);
 }
 
   changeMarkerClicked(markerNum) {
     if (!markerNum && markerNum !== 0) {markerNum = false}
     this.setState({markerClicked: markerNum});
   }
+  changeShownMarkers(markers) {
+    this.setState({shownMarkers: markers});
+  }
+  filterMarkers(filterInput) {
+    let markers = this.state.Markers;
+    if (filterInput !== '') {
+      const regex = new RegExp(filterInput, 'i')
+      markers = markers.filter(marker => regex.test(marker.name) )
+    }
+    this.changeShownMarkers(markers);
+  }
 
   render() {
     return (
       <div>
       <SideList
-        markers={this.state.Markers}
+        markers={this.state.shownMarkers || this.state.Markers}
         changeMarkerClicked={this.changeMarkerClicked}
+        filterMarkers={this.filterMarkers}
         />
       <GoogleMapSection
         googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCAQb9xq2iRT6lG8DW3cGP1K43kastziMA"
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100vh`, width: `80vw`}} />}
         mapElement={<div style={{ height: `100%` }} />}
-        markers={this.state.Markers}
+        markers={this.state.shownMarkers || this.state.Markers}
         includeMarkerInBounds={this.includeMarkerInBounds}
         changeMarkerClicked={this.changeMarkerClicked}
       />
@@ -61,14 +77,30 @@ class App extends Component {
 }
 
 class SideList extends Component {
+  state = {
+    filterInput: ''
+  }
+
+  setFilterInput = (event) => {
+    let filterInput = event.target.value;
+    this.setState({filterInput: filterInput});
+    this.props.filterMarkers(filterInput);
+  }
+
   render() {
-    return <ul>
+    return <div>
+      <DebounceInput
+        onChange={this.setFilterInput}
+        value={this.state.setFilterInput}
+        debounceTimeout={200}
+      />
+      <ul>
       {this.props.markers.map((marker, index) => <li
         key={'marker-onlist-'+index}
         onClick={() => this.props.changeMarkerClicked(index)}>
           <button onClick={(e) => {e.preventDefault()}}>{marker.name}</button>
       </li>)}
-    </ul>
+    </ul></div>
   }
 }
 
@@ -82,63 +114,65 @@ class GoogleMapContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        bounds: new window.google.maps.LatLngBounds(),
-        areBoundsBeingChanged: false,
-        timesBoundsChanged: 0
+        zoom: 17
       }
-    this.includeMarkerInBounds = this.includeMarkerInBounds.bind(this);
-    this.checkIfBoundsAreBeingChanged = this.checkIfBoundsAreBeingChanged.bind(this);
   }
 
-  checkIfBoundsAreBeingChanged() {
-    let here = this;
-    return new Promise(function (resolve, reject) {
-        (function waitForFalse(){
-            if (here.state.areBoundsBeingChanged === false) return resolve();
-            setTimeout(waitForFalse, 30);
-            return reject('bounds are being changed');
-        })();
-      });
-    }
-
-  includeMarkerInBounds(marker) {
-    let bounds;
-    const ifBoundsHaveFinishedChanging = this.checkIfBoundsAreBeingChanged;
-    return ifBoundsHaveFinishedChanging().then(response => {
-      this.setState({areBoundsBeingChanged: true});
-      bounds = this.state.bounds;
-      bounds.extend(marker.props.position);
-      return this.setState(prevState => { return {bounds: bounds, areBoundsBeingChanged: false,
-        timesBoundsChanged: ++prevState.timesBoundsChanged} }, () => {
-          if (this.state.timesBoundsChanged === this.props.markers.length) {
-            this._mapRef.fitBounds(this.state.bounds);
-          }
-        });
+  includeMarkersInBounds() {
+    if(this.props.markers.length < 2) {return}
+    let bounds = new window.google.maps.LatLngBounds();
+    const centerOfTown = {lat:32.080748, lng:34.781330};
+      bounds.extend(centerOfTown);
+      this.props.markers.map(marker => {
+        bounds.extend({lat: marker.lat, lng: marker.lng})
       })
+      this.checkIfMapRefExists().then(
+        res => {
+          this._mapRef.fitBounds(bounds);
+        }
+      )
     }
 
-    saveMapRef = (ref) => {
+  checkIfMapRefExists() {
+    let here = this;
+    return new Promise(function(resolve, reject) {
+      (function waitForMapRef() {
+        if (here._mapRef) {return resolve()}
+        return setTimeout(waitForMapRef, 30);
+      })()
+      })
+  }
+
+  saveMapRef = (ref) => {
       if (!ref || this._mapRef) return;
       this._mapRef = ref;
     }
 
+
   render() {
     return (
       <GoogleMap
-        defaultZoom={17}
+        defaultZoom={this.state.zoom}
+        zoom={this.state.zoom}
         defaultCenter={{ lat: 32.080359, lng: 34.780670 }}
         ref={this.saveMapRef}
         >
           {
-            this.props.markers.map((marker, index) => <Marker
-            onClick={() => this.props.changeMarkerClicked(index)}
-            position={{lat: marker.lat, lng: marker.lng}}
-            ref={this.includeMarkerInBounds}
-            key={'marker-onmap-'+index}
-            />
+            this.props.markers.map((marker, index, markers) => {
+              const position = {lat: marker.lat, lng: marker.lng}
+              if (marker === markers[markers.length-1]) {
+                this.includeMarkersInBounds();
+              }
+              return <Marker
+                key={'marker-onmap-'+index}
+                onClick={() => this.props.changeMarkerClicked(index)}
+                position={position}
+                ref={this.includeMarkerInBounds}
+              />
+            }
           )
        }
-      </GoogleMap>
+    </GoogleMap>
     ) }
 }
 
